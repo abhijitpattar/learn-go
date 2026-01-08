a3d2d5029853d050bdcef744e400d14d7f49e3ba
Abhijit Pattar <abhijit.pattar@gmail.com>
2026-01-08 19:53:12 +0530

    profit calc

--- app.go
+++ app.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello go")
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Revenue : ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Expenses : ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Tax Rate : ")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+
+	fmt.Println("Earnings Before Profit - ", ebt)
+	fmt.Println("Profit - ", profit)
+	fmt.Println("Profit ratio - ", ratio)
 }
